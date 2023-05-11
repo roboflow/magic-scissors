b45f917cfd5959175dd218303ab5b7802b709315
Brad Dwyer <brad@roboflow.com>
2023-05-11 10:28:40 -0700

    Fix infinite loop when object is bigger than dropzone

--- image-processing/src/generate_image.py
+++ image-processing/src/generate_image.py
@@ -33,6 +33,11 @@ def copy_paste(
     source_height, source_width = source_image.shape[0:2]
     mask = polygon_to_mask(source_polygon, (source_width, source_height))
 
+    #if the source is going to be bigger than the target, then we need to scale it down so it will fit
+    if source_width > target_image.shape[1] or source_height > target_image.shape[0]:
+        scale = min(target_image.shape[1] / source_width, target_image.shape[0] / source_height)
+        # print("scaling source image down to fit", scale)
+
     # scale the source and mask
     scaled_source = cv2.resize(source_image, None, fx=scale, fy=scale)
     scaled_mask = cv2.resize(mask, None, fx=scale, fy=scale)
@@ -56,9 +61,13 @@ def copy_paste(
 def random_point_in_background(bg, max_x, max_y):
     polygon = Polygon(bg.polygon)
     minx, miny, maxx, maxy = polygon.bounds
+    # print("polygon bounds", minx, miny, maxx, maxy)
+
     maxx = min(maxx, max_x)
     maxy = min(maxy, max_y)
 
+    # print("target max x, y", max_x, max_y)
+    # print("picked max x, y", maxx, maxy)
     attempt = 0
     # print("get random point in background", maxx, maxy)
     while True:
@@ -66,8 +75,8 @@ def random_point_in_background(bg, max_x, max_y):
         if polygon.contains(pnt):
             # print("found random point", pnt, pnt.x, pnt.y)
             return [pnt.x, pnt.y]
+        else:
+            attempt += 1
         if attempt > 100:
-            return [
-                np.random.uniform(0, max_x),
-                np.random.uniform(0, max_y),
-            ]
+            # print("failed to find random point in background", minx, miny, maxx, maxy, max_x, max_y)
+            return [None, None]
